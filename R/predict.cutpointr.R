740b4f9e5f7805f46b259eea90ff86431364affd
C. Thiele <c.thiele@gmx-topmail.de>
2016-12-14 17:06:29 +0100

    Recognize NSE transformations of indep. var

--- R/predict.cutpointr.R
+++ R/predict.cutpointr.R
@@ -1,14 +1,11 @@
 #' @export
-predict.cutpointr <- function(cutpointr, newdata = NULL) {
-    if (is.null(newdata)) {
-        newdata <- cutpointr$data
-    }
-    outcome_name   <- cutpointr$outcome[1]
+predict.cutpointr <- function(cutpointr, newdata) {
     predictor_name <- cutpointr$predictor[1]
+    # The predictor may have been altered using NSE
+    indep_var <- eval(parse(text = predictor_name), newdata, parent.frame())
     pos_class      <- cutpointr$pos_class[1]
     neg_class      <- cutpointr$neg_class[1]
 
-    # Cutpoint by group
     if (any(colnames(cutpointr) == "group")) {
         grouping_name <- unique(cutpointr$grouping)
         if(!any(colnames(newdata) == grouping_name)) stop("grouping variable not found in newdata")
@@ -18,19 +15,22 @@ predict.cutpointr <- function(cutpointr, newdata = NULL) {
         })
         optimal_cuts <- cutpointr$optimal_cutpoint[opt_cut_ind]
         if (cutpointr$direction[1] == ">") {
-            preds <- newdata[, predictor_name] > optimal_cuts
+            # preds <- newdata[, predictor_name] > optimal_cuts
+            preds <- indep_var > optimal_cuts
             preds <- ifelse(preds, pos_class, neg_class)
         } else if (cutpointr$direction[1] == "<") {
-            preds <- newdata[, predictor_name] < optimal_cuts
+            preds <- indep_var < optimal_cuts
             preds <- ifelse(preds, pos_class, neg_class)
         }
     } else {
         optimal_cut <- cutpointr$optimal_cutpoint
         if (cutpointr$direction[1] == ">") {
-            preds <- newdata[, predictor_name] > optimal_cut
+            # preds <- newdata[, predictor_name] > optimal_cut
+            preds <- indep_var > optimal_cut
             preds <- ifelse(preds, pos_class, neg_class)
         } else if (cutpointr$direction[1] == "<") {
-            preds <- newdata[, predictor_name] < optimal_cut
+            # preds <- newdata[, predictor_name] < optimal_cut
+            preds <- indep_var < optimal_cut
             preds <- ifelse(preds, pos_class, neg_class)
         }
     }
