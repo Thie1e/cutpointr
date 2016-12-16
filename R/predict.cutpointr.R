9c3773088a469f793a2d3375ce033816853333b9
thie1e <c.thiele@gmx-topmail.de>
2016-12-16 18:57:11 +0100

    Enable NSE for subgroup variable

--- R/predict.cutpointr.R
+++ R/predict.cutpointr.R
@@ -3,17 +3,25 @@ predict.cutpointr <- function(cutpointr, newdata) {
     predictor_name <- cutpointr$predictor[1]
     # The predictor may have been altered using NSE
     indep_var <- eval(parse(text = predictor_name), newdata, parent.frame())
-    pos_class      <- cutpointr$pos_class[1]
-    neg_class      <- cutpointr$neg_class[1]
+    pos_class <- cutpointr$pos_class[1]
+    neg_class <- cutpointr$neg_class[1]
 
     if (any(colnames(cutpointr) == "group")) {
-        grouping_name <- unique(cutpointr$grouping)
-        if(!any(colnames(newdata) == grouping_name)) stop("grouping variable not found in newdata")
-        optimal_cut <- cutpointr$optimal_cutpoint[cutpointr$group == newdata$group]
-        opt_cut_ind <- purrr::map_int(newdata[, grouping_name], function(g) {
-            which(opt_cut$group == g)
+        grouping_name     <- unique(cutpointr$grouping)
+        grouping_var_new  <- eval(parse(text = grouping_name), newdata, parent.frame())
+        opt_cut_ind <- purrr::map_int(grouping_var_new, function(g) {
+            which(cutpointr$group == g)
         })
         optimal_cuts <- cutpointr$optimal_cutpoint[opt_cut_ind]
+
+        #-------
+        # if(!any(colnames(newdata) == grouping_name)) stop("grouping variable not found in newdata")
+        # optimal_cut <- cutpointr$optimal_cutpoint[cutpointr$group == newdata$group]
+        # opt_cut_ind <- purrr::map_int(newdata[, grouping_name], function(g) {
+        #     which(cutpointr$group == g)
+        # })
+        # optimal_cuts <- cutpointr$optimal_cutpoint[opt_cut_ind]
+        #----------
         if (cutpointr$direction[1] == ">") {
             # preds <- newdata[, predictor_name] > optimal_cuts
             preds <- indep_var > optimal_cuts
