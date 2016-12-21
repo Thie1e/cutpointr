0deed1d6b04cb64c07c51d381209c66343c8b95c
thie1e <c.thiele@gmx-topmail.de>
2016-12-21 18:40:21 +0100

    Prevent flipping when only 2 plots

--- R/grid_arrange_shared_legend.R
+++ R/grid_arrange_shared_legend.R
@@ -27,9 +27,14 @@ grid_arrange_shared_legend <- function(plots, ncol = length(plots), nrow = 1,
                                                widths = wid)
         }
     } else {
-        # No legend because only one class:
-        combined <- gridExtra::arrangeGrob(do.call(gridExtra::arrangeGrob, gl),
-                                           nrow = 1, ncol = 1)
+        # No legend because only one class. Different methods to plot ROC
+        # beneath dist, not under, if length(gl) == 2
+        if (length(gl) == 2) {
+            combined <- gridExtra::grid.arrange(gl[[1]], gl[[2]], ncol = 2, nrow = 1)
+        } else {
+            combined <- gridExtra::arrangeGrob(do.call(gridExtra::arrangeGrob, gl),
+                                               ncol = 1, nrow = 1)
+        }
     }
     grid::grid.newpage()
     grid::grid.draw(combined)
