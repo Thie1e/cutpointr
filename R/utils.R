1fe5f8e14836353b2e582db137c5767011498c3c
thie1e <c.thiele@gmx-topmail.de>
2017-08-09 18:44:19 +0200

    Add some utility functions for checks and finding the metric name

--- R/utils.R
+++ R/utils.R
@@ -12,13 +12,32 @@ find_metric_name <- function(object) {
     }
 }
 
+find_metric_name_boot <- function(object) {
+    if ("subgroup" %in% colnames(object)) {
+        return(colnames(object)[4])
+    } else {
+        return(colnames(object)[3])
+    }
+}
+
 check_colnames <- function(metric_name) {
     default_cols <- c("subgroup", "direction", "optimal_cutpoint",
                      "method", "accuracy", "sensitivity", "specificity", "AUC",
                      "pos_class", "neg_class", "prevalence",
                      "outcome", "predictor", "grouping", "data", "roc_curve")
     if (metric_name %in% default_cols)
-        stop("The metric function should return a matrix with a colname that differs from the default colnames of cutpointr to avoid duplicates")
+        stop(paste("The metric function should return a matrix or data.frame",
+                   "with a colname that differs from the default colnames of",
+                   "cutpointr to avoid duplicates"))
+}
+
+validate_colnames <- function(coln) {
+    valid_names <- make.names(coln)
+    valid_names[valid_names == "NULL."] <- "NULL"
+    invalid_names <- valid_names != coln
+    if (any(invalid_names)) {
+        warning(paste("Invalid column names:", coln[invalid_names]))
+    }
 }
 
 ifel_pos_neg <- function(logi_vec, pos_class, neg_class) {
@@ -28,7 +47,6 @@ ifel_pos_neg <- function(logi_vec, pos_class, neg_class) {
 }
 
 midpoint <- function(oc, x, direction) {
-    #if (oc > max(x) | oc < min(x))
     x <- c(oc, x)
     if (direction == ">=") {
         x <- sort(unique(x))
@@ -54,3 +72,35 @@ get_opt_ind <- function(roc_curve, oc, direction) {
 summary_sd <- function(x) {
     c(summary(x)[1:6], SD = stats::sd(x, na.rm = TRUE))
 }
+
+# If the output of the metric function is no named matrix with one column,
+# convert it to one. Also run some checks.
+sanitize_metric <- function(m, m_name, n) {
+    if ("data.frame" %in% class(m)) {
+        m <- as.matrix(m)
+    }
+    if (!is.null(dim(m))) {
+        if (dim(m)[2] == 1 & class(m) == "matrix") {
+            res <- m
+        } else {
+            stop(paste("The metric function should return a numeric vector",
+                       "or a one-column matrix or data.frame."))
+        }
+    } else if (is.numeric(m)) {
+        res <- matrix(m, ncol = 1, dimnames = list(NULL, m_name))
+    } else {
+        stop(paste("Can't process metric of type", class(m)))
+    }
+    finite_res <- is.finite(res)
+    if (any(!finite_res)) {
+        message("Converting infinite metric values to NA")
+        res[!finite_res] <- NA
+    }
+    if (nrow(res) != n) {
+        stop("Number of returned metric values not equal to n")
+    }
+    colnames(res) <- make.names(colnames(res))
+    return(res)
+}
+
+
