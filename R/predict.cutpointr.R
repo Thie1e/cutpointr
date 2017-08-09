de55edfb8743fcdcd6484aa284a71b0b2c89d4d6
thie1e <c.thiele@gmx-topmail.de>
2017-08-09 18:43:49 +0200

    Add documentation for predict.cutpointr

--- R/predict.cutpointr.R
+++ R/predict.cutpointr.R
@@ -1,5 +1,23 @@
+#' Predict using a cutpointr object
+#'
+#' Predictions are made on the data.frame in newdata. That data.frame should
+#' contain a column with a name that is equal to the predictor name in
+#' the cutpointr object. The class of the output is identical to the class
+#' of the predictor.
+#'
+#' @param object a cutpointr object.
+#' @param newdata a data.frame with a column that contains the predictor
+#' variable
+#' @param ... further arguments.
+#' @examples
+#' oc <- cutpointr(suicide, dsi, suicide)
+#' ## Return in-sample predictions
+#' predict(oc, newdata = data.frame(dsi = oc$data[[1]]$dsi))
 #' @export
 predict.cutpointr <- function(object, newdata, ...) {
+    if (!("data.frame" %in% class(newdata))) {
+        stop("newdata should be a data.frame with a correctly named predictor column")
+    }
     predictor_name <- object$predictor[1]
     # The predictor may have been altered using NSE
     indep_var <- eval(parse(text = predictor_name), newdata, parent.frame())
@@ -14,7 +32,6 @@ predict.cutpointr <- function(object, newdata, ...) {
         })
         optimal_cuts <- object$optimal_cutpoint[opt_cut_ind]
         if (object$direction[1] == ">=") {
-            # preds <- newdata[, predictor_name] > optimal_cuts
             preds <- indep_var >= optimal_cuts
             preds <- ifel_pos_neg(preds, pos_class, neg_class)
         } else if (object$direction[1] == "<=") {
@@ -24,11 +41,9 @@ predict.cutpointr <- function(object, newdata, ...) {
     } else {
         optimal_cut <- object$optimal_cutpoint
         if (object$direction[1] == ">=") {
-            # preds <- newdata[, predictor_name] > optimal_cut
             preds <- indep_var >= optimal_cut
             preds <- ifel_pos_neg(preds, pos_class, neg_class)
         } else if (object$direction[1] == "<=") {
-            # preds <- newdata[, predictor_name] < optimal_cut
             preds <- indep_var <= optimal_cut
             preds <- ifel_pos_neg(preds, pos_class, neg_class)
         }
