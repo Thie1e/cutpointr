8a6242d052e9e6fd377d6c34fda38e13e7241ebd
thie1e <c.thiele@gmx-topmail.de>
2017-08-09 18:43:05 +0200

    Enable plotting a bootstrapped confidence interval

--- R/plot_metric.R
+++ R/plot_metric.R
@@ -6,6 +6,8 @@
 #' value for every cutpoint along with the optimal cutpoint.
 #'
 #' @param x A cutpointr object.
+#' @param conf_lvl The confidence level of the bootstrap confidence interval.
+#' Set to 0 to draw no bootstrap confidence interval.
 #' @examples
 #' set.seed(123)
 #' class_a <- rnorm(50, mean = 10)
@@ -19,38 +21,96 @@
 #' opt_cut <- cutpointr(dat, x, y, method = maximize_metric, metric = youden,
 #'   pos_class = 1, direction = ">=")
 #' plot_metric(opt_cut)
+#' @importFrom dplyr %>%
 #' @export
-plot_metric <- function(x) {
+plot_metric <- function(x, conf_lvl = 0.95) {
     stopifnot("cutpointr" %in% class(x))
     if (suppressWarnings(is.null(x$roc_curve[[1]]$m))) {
         stop(paste("The cutpointr object does not include a metric column in",
                    "roc_curve - maybe because a method other than",
                    "maximize_metric or minimize_metric was used"))
     }
+
+    if ("boot" %in% colnames(x)) {
+        if ("subgroup" %in% colnames(x)) {
+            roc_b_unnested <- x %>%
+                dplyr::select_(.dots = c("boot", "subgroup")) %>%
+                tidyr::unnest_(unnest_cols = "boot") %>%
+                dplyr::select_(.dots = c("subgroup", "roc_curve_b")) %>%
+                tidyr::unnest_(unnest_cols = "roc_curve_b")
+            roc_b_unnested <- roc_b_unnested[is.finite(roc_b_unnested$x.sorted), ]
+            roc_b_unnested <- roc_b_unnested %>%
+                dplyr::select_(.dots = c("x.sorted", "m", "subgroup")) %>%
+                dplyr::group_by_(.dots = c("x.sorted", "subgroup")) %>%
+                dplyr::summarise_(ymin = ~ quantile(m, (1 - conf_lvl) / 2),
+                                  ymax = ~ quantile(m, 1 - (1 - conf_lvl) / 2))
+        } else {
+            roc_b_unnested <- x$boot[[1]] %>%
+                tidyr::unnest_(unnest_cols = "roc_curve_b")
+            roc_b_unnested <- roc_b_unnested[is.finite(roc_b_unnested$x.sorted), ]
+            roc_b_unnested <- roc_b_unnested %>%
+                dplyr::select_(.dots = c("x.sorted", "m")) %>%
+                dplyr::group_by_(.dots = "x.sorted") %>%
+                dplyr::summarise_(ymin = ~ quantile(m, (1 - conf_lvl) / 2),
+                                  ymax = ~ quantile(m, 1 - (1 - conf_lvl) / 2))
+        }
+    }
     metric_name <- find_metric_name(x)
     if ("subgroup" %in% colnames(x)) {
         res_unnested <- x %>%
             dplyr::select_(.dots = c("roc_curve", "subgroup")) %>%
             tidyr::unnest_(unnest_cols = "roc_curve")
-        # After LOESS smoothing metric is NA where x = Inf or -Inf
-        res_unnested <- stats::na.omit(res_unnested)
-        p <- ggplot2::ggplot(res_unnested, ggplot2::aes_(x = ~ x.sorted,
-                                                         y = ~ m,
-                                                         color = ~ subgroup)) +
-            ggplot2::geom_line() + ggplot2::geom_point() +
-            ggplot2::ggtitle("Metric values by cutpoint value") +
-            ggplot2::ylab(metric_name) + ggplot2::xlab("Cutpoint")
+        res_unnested <- res_unnested[is.finite(res_unnested$x.sorted),
+                                     c("x.sorted", "m", "subgroup")]
+        if ("boot" %in% colnames(x)) {
+            res_unnested <- merge(res_unnested,
+                                  roc_b_unnested[, c("subgroup", "x.sorted", "ymin", "ymax")],
+                                  by = c("x.sorted", "subgroup"))
+            p <- ggplot2::ggplot(res_unnested, ggplot2::aes_(x = ~ x.sorted,
+                                                             y = ~ m,
+                                                             ymin = ~ ymin,
+                                                             ymax = ~ ymax,
+                                                             color = ~ subgroup,
+                                                             fill = ~ subgroup)) +
+                ggplot2::geom_line() +
+                ggplot2::geom_point() +
+                ggplot2::ggtitle("Metric values by cutpoint value") +
+                ggplot2::ylab(metric_name) + ggplot2::xlab("Cutpoint") +
+                ggplot2::geom_ribbon(alpha = 0.2)
+        } else {
+            p <- ggplot2::ggplot(res_unnested, ggplot2::aes_(x = ~ x.sorted,
+                                                                   y = ~ m,
+                                                                   color = ~ subgroup)) +
+                ggplot2::geom_line() + ggplot2::geom_point() +
+                ggplot2::ggtitle("Metric values by cutpoint value") +
+                ggplot2::ylab(metric_name) + ggplot2::xlab("Cutpoint")
+        }
     } else {
         res_unnested <- x %>%
             dplyr::select_(.dots = "roc_curve") %>%
             tidyr::unnest_(unnest_cols = "roc_curve")
-        # After LOESS smoothing metric is NA where x = Inf or -Inf
-        res_unnested <- stats::na.omit(res_unnested)
-        p <- ggplot2::ggplot(res_unnested, ggplot2::aes_(x = ~ x.sorted,
-                                                         y = ~ m)) +
-            ggplot2::geom_line() + ggplot2::geom_point() +
-            ggplot2::ggtitle("Metric values by cutpoint value") +
-            ggplot2::ylab(metric_name) + ggplot2::xlab("Cutpoint")
+        res_unnested <- res_unnested[is.finite(res_unnested$x.sorted),
+                                     (c("x.sorted", "m"))]
+        if ("boot" %in% colnames(x)) {
+            res_unnested <- merge(res_unnested,
+                                  roc_b_unnested[, c("x.sorted", "ymin", "ymax")],
+                                  by = "x.sorted")
+            p <- ggplot2::ggplot(res_unnested, ggplot2::aes_(x = ~ x.sorted,
+                                                             y = ~ m,
+                                                             ymax = ~ ymax,
+                                                             ymin = ~ ymin)) +
+                ggplot2::geom_line() + ggplot2::geom_point() +
+                ggplot2::ggtitle("Metric values by cutpoint value") +
+                ggplot2::ylab(metric_name) + ggplot2::xlab("Cutpoint") +
+                ggplot2::geom_ribbon(alpha = 0.2)
+        } else {
+            p <- ggplot2::ggplot(res_unnested, ggplot2::aes_(x = ~ x.sorted,
+                                                             y = ~ m)) +
+                ggplot2::geom_line() + ggplot2::geom_point() +
+                ggplot2::ggtitle("Metric values by cutpoint value") +
+                ggplot2::ylab(metric_name) + ggplot2::xlab("Cutpoint")
+        }
     }
     return(p)
 }
+
