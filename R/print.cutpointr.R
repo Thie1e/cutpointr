2e72a740f76da0bb9998f2f0946dd277f733bad4
Thie1e <c.thiele@gmx-topmail.de>
2018-08-09 16:06:27 +0200

    Add sigfig argument and set default to 6

--- R/print.cutpointr.R
+++ R/print.cutpointr.R
@@ -7,12 +7,17 @@
 #' @param x a cutpointr object.
 #' @param width width of output.
 #' @param n number of rows to print.
+#' @param sigfig Number of significant digits to print. Temporarily
+#' overrides options("pillar.sigfig").
 #' @param ... further arguments.
 #' @export
-print.cutpointr <- function(x, width = 1000, n = 50, ...) {
+print.cutpointr <- function(x, width = 1000, n = 50, sigfig = 6, ...) {
     # print.tbl_df is not exported by tibble, avoid :::
     class(x) <- c("tbl_df", "tbl", "data.frame")
+    old_sigfig <- options("pillar.sigfig")
+    options(pillar.sigfig = sigfig)
     print(x, width = width, n = n)
+    options(pillar.sigfig = old_sigfig$pillar.sigfig)
     class(x) <- c("cutpointr", "tbl_df", "tbl", "data.frame")
     invisible(x)
 }
