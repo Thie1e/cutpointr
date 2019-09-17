46529945528e1706234576aae7fa9baf93d68acd
Christian Thiele <c.thiele@gmx-topmail.de>
2019-09-17 14:37:39 +0200

    Same interface as in boot_test

--- R/boot_ci.R
+++ R/boot_ci.R
@@ -14,19 +14,20 @@
 #' @param variable Variable to calculate CI for
 #' @param alpha Alpha level. Quantiles of the bootstrapped values are returned
 #' for (alpha / 2) and 1 - (alpha / 2).
+#' @param in_bag Whether the in-bag or out-of-bag results should be used for testing
 #' @return A data frame with the columns quantile and value
 #' @examples
 #' \dontrun{
 #' opt_cut <- cutpointr(suicide, dsi, suicide, gender,
 #'   metric = youden, boot_runs = 1000)
-#' boot_ci(opt_cut, optimal_cutpoint, 0.05)
-#' boot_ci(opt_cut, acc_oob, 0.05)
-#' boot_ci(opt_cut, cohens_kappa_oob, 0.05)
-#' boot_ci(opt_cut, AUC_b, 0.05)
+#' boot_ci(opt_cut, optimal_cutpoint, in_bag = FALSE, alpha = 0.05)
+#' boot_ci(opt_cut, acc, in_bag = FALSE, alpha = 0.05)
+#' boot_ci(opt_cut, cohens_kappa, in_bag = FALSE, alpha = 0.05)
+#' boot_ci(opt_cut, AUC, in_bag = TRUE, alpha = 0.05)
 #' }
 #' @export
 #' @family main cutpointr functions
-boot_ci <- function(x, variable, alpha = 0.05) {
+boot_ci <- function(x, variable, in_bag = TRUE, alpha = 0.05) {
     if (alpha < 0 | alpha > 1) stop("alpha should be between 0 and 1.")
     if (!inherits(x, "cutpointr")) {
         stop("Only objects of type cutpointr are supported")
@@ -36,9 +37,18 @@ boot_ci <- function(x, variable, alpha = 0.05) {
     }
 
     if (!("subgroup" %in% colnames(x))) {
-        variable_sym <- rlang::ensym(variable)
-        variable_expr <- rlang::enexpr(variable_sym)
-        variable <- rlang::eval_tidy(expr = variable_expr, data = x$boot[[1]])
+        variable <- rlang::enquo(variable)
+        variable <- rlang::as_name(variable)
+        if (in_bag) suffix <- "_b" else suffix <- "_oob"
+        if (variable == "optimal_cutpoint") {
+            suffix <- ""
+            in_bag = TRUE
+        }
+        variable <- paste0(variable, suffix)
+        variable <- x %>%
+            dplyr::select(.data$boot) %>%
+            tidyr::unnest(cols = .data$boot) %>%
+            dplyr::pull(variable)
         values <- stats::quantile(variable, probs = c(alpha / 2, 1 - alpha / 2),
                            na.rm = TRUE)
         res <- tibble::tibble(
@@ -49,9 +59,16 @@ boot_ci <- function(x, variable, alpha = 0.05) {
     } else {
         res <- purrr::map2_dfr(.x = x$subgroup, .y = x$boot,
                                .f = function(s, b) {
-            variable_sym <- rlang::ensym(variable)
-            variable_expr <- rlang::enexpr(variable_sym)
-            variable <- rlang::eval_tidy(expr = variable_expr, data = b)
+            variable <- rlang::enquo(variable)
+            variable <- rlang::as_name(variable)
+            if (in_bag) suffix <- "_b" else suffix <- "_oob"
+            if (variable == "optimal_cutpoint") {
+                suffix <- ""
+                in_bag = TRUE
+            }
+            variable <- paste0(variable, suffix)
+            variable <- b %>%
+                dplyr::pull(variable)
             values <- stats::quantile(variable, probs = c(alpha / 2, 1 - alpha / 2),
                                na.rm = TRUE)
             tibble::tibble(
